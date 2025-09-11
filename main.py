b24336c0ffc3f37421486831915cf89afbac67ba
SirobO49 <boris.ohmer@gmx.de>
2025-09-11 17:41:09 +0200

    increased epochs to 13

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
     train(model, train_loader, val_loader, optimizer, criterion,
           dataset=vocab_ds,
-          num_epochs=8,
+          num_epochs=13,
           pad_idx=pad_idx,
           teacher_forcing_ratio= 0.5)
 
