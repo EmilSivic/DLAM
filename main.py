9bcb33d8c88517ad7e1b2206f924f5d486e2f3a7
SirobO49 <boris.ohmer@gmx.de>
2025-09-11 23:19:11 +0200

    adjusted schechduler

--- main.py
+++ main.py
@@ -234,8 +234,7 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss(ignore_index=pad_idx, label_smoothing=0.1)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode="min", factor=0.5, patience=3, verbose=True
-    )
+        optimizer, mode="min", factor=0.5, patience=3)
     train(model, train_loader, val_loader, optimizer, criterion,
           dataset=vocab_ds,
           num_epochs=30,
