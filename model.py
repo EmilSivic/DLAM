72411a0a79e8621dac5a0fa70edfcf512f628a65
SirobO49 <boris.ohmer@gmx.de>
2025-09-12 15:31:08 +0200

    LSTM Large: Embedding_dim = 512, hidden_dim = 256, Layers = 2, increased batch_size from 128 to 256, reduced dropout from 0.5 to 0.3. Additionally made the Learning Rate schedular more aggressive (factor=0.3, patience=2). BIDRECTIONAL=True

--- model.py
+++ model.py
@@ -3,19 +3,28 @@ import torch.nn as nn
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_vocab_size, embedding_dim, hidden_dim, num_layers=2, dropout=0.3):
+    def __init__(self, input_vocab_size, embedding_dim, hidden_dim, num_layers=2, dropout=0.3, bidirectional=True):
         super().__init__()
         self.embedding = nn.Embedding(input_vocab_size, embedding_dim)
-        self.emb_dropout = nn.Dropout(dropout)   #dropout on embeddings
+        self.emb_dropout = nn.Dropout(dropout)   # dropout on embeddings
         self.lstm = nn.LSTM(
             embedding_dim,
             hidden_dim,
             num_layers=num_layers,
             batch_first=True,
-            dropout=dropout if num_layers > 1 else 0.0
+            dropout=dropout if num_layers > 1 else 0.0,
+            bidirectional=bidirectional
         )
 
-        # --- Speichere Hyperparameter ---
+        self.bidirectional = bidirectional
+        self.num_directions = 2 if bidirectional else 1
+
+        # falls bidirectional: Linear-Layer um hidden_dim*2 -> hidden_dim zu reduzieren
+        if bidirectional:
+            self.reduce_h = nn.Linear(hidden_dim * 2, hidden_dim)
+            self.reduce_c = nn.Linear(hidden_dim * 2, hidden_dim)
+
+        # save hyperparameters
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
@@ -23,11 +32,18 @@ class EncoderRNN(nn.Module):
 
     def forward(self, input_ids, input_lengths):
         embedded = self.embedding(input_ids)
-        embedded = self.emb_dropout(embedded)    #apply dropout
+        embedded = self.emb_dropout(embedded)    # apply dropout
         packed = nn.utils.rnn.pack_padded_sequence(
             embedded, input_lengths.cpu(), batch_first=True, enforce_sorted=False
         )
         outputs, (hidden, cell) = self.lstm(packed)
+
+        if self.bidirectional:
+            # letzte Layer der beiden Richtungen zusammenfassen
+            hidden = torch.cat((hidden[-2], hidden[-1]), dim=1)
+            cell   = torch.cat((cell[-2], cell[-1]), dim=1)
+            hidden = self.reduce_h(hidden).unsqueeze(0)
+            cell   = self.reduce_c(cell).unsqueeze(0)
         return hidden, cell
 
 
@@ -46,8 +62,7 @@ class DecoderRNN(nn.Module):
         self.fc_out = nn.Linear(hidden_dim, output_vocab_size)
         self.out_dropout = nn.Dropout(dropout)   # dropout on projection
 
-
-        # --- Speichere Hyperparameter ---
+        # save hyperparams
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
