3192957547a21b4b4b2d0fa955020de874ab2781
SirobO49 <boris.ohmer@gmx.de>
2025-09-15 15:07:21 +0200

    lower epochs

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ if __name__ == "__main__":
 
     train(model, train_loader, val_loader, optimizer, criterion,
           dataset=vocab_ds,
-          num_epochs=20,
+          num_epochs=2,
           pad_idx=pad_idx,
           teacher_forcing_ratio=0.5)
 
