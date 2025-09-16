2be20dee5bb7b6bc5e61dfb42c0021fca9defdcb
SirobO49 <boris.ohmer@gmx.de>
2025-09-17 00:03:32 +0200

    ..

--- transformer/transformer_main_tuned.py
+++ transformer/transformer_main_tuned.py
@@ -1,3 +1,10 @@
+import sys, os
+
+# Add project root to sys.path 
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(FILE_DIR, ".."))
+sys.path.insert(0, ROOT_DIR)
+
 import sys, os, math, torch
 import torch.nn as nn
 import torch.optim as optim
